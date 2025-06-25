32a7c71f2f4d0fb0fa265ca4261134b2cf63e8c8
Jonas Fortes <jonascforte@hotmail.com>
2025-06-25 19:01:00 -0300

    feat: dynamic rendering map

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,6 +1,11 @@
 'use client'
-import Map from "@/components/Map"
+// import Map from "@/components/Map"
+import dynamic from "next/dynamic"
 
+const Map = dynamic(
+    ()=> import('@/components/Map'),
+    {ssr:false}
+)
 
 export default function MapPage() {
     return (
