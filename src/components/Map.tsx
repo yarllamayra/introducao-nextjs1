307575aba3dc5c290f00af78e246b478eba3c73f
Jonas Fortes <jonascforte@hotmail.com>
2025-07-14 21:16:30 -0300

    feat: get all places

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -3,26 +3,27 @@
 import { MapContainer, TileLayer, Marker, Popup, useMapEvent, useMap} from 'react-leaflet';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import PlaceForm from './PlaceForm';
+import axios from 'axios';
 
-type PointsType = 'restaurant' | 'bar' | 'hotel';
+type PointsType = "RESTAURANTE" | "BAR" | "HOTEL"
 
 // Mapeamento dos ícones por tipo
 const iconMap: Record<PointsType, L.Icon> = {
-  restaurant: new L.Icon({
+  RESTAURANTE: new L.Icon({
     iconUrl: 'https://cdn-icons-png.flaticon.com/512/948/948036.png',
     iconSize: [40, 40],
     iconAnchor: [20, 40],
     popupAnchor: [0, -40],
   }),
-  bar: new L.Icon({
+  BAR: new L.Icon({
     iconUrl: 'https://cdn-icons-png.flaticon.com/512/2907/2907457.png',
     iconSize: [40, 40],
     iconAnchor: [20, 40],
     popupAnchor: [0, -40],
   }),
-  hotel: new L.Icon({
+  HOTEL: new L.Icon({
     iconUrl: 'https://cdn-icons-png.flaticon.com/512/3009/3009489.png',
     iconSize: [40, 40],
     iconAnchor: [20, 40],
@@ -30,41 +31,42 @@ const iconMap: Record<PointsType, L.Icon> = {
   }),
 };
 
-type Point = {
+type Place = {
   id: number;
   name: string;
-  position: [number, number];
-  image: string;
-  description: string;
   type: PointsType;
+  phone: string;
+  latitude: number;
+  longitude: number;
+  images: {url:string; public_id:string}[];
 };
 
-const points: Point[] = [
-  {
-    id: 1,
-    name: 'Restaurante Saboroso',
-    position: [-3.024930, -39.649836],
-    image: 'https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    description: 'Um restaurante incrível no centro.',
-    type: 'restaurant',
-  },
-  {
-    id: 2,
-    name: 'Bar do Zé',
-    position: [-3.024860, -39.643643],
-    image: 'https://plus.unsplash.com/premium_photo-1661730134261-4381db9b5de3?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    description: 'O melhor happy hour da cidade.',
-    type: 'bar',
-  },
-  {
-    id: 3,
-    name: 'Hotel Confort',
-    position: [-3.034040, -39.654925],
-    image: 'https://images.unsplash.com/photo-1551882547-ff40c63fe5fa?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    description: 'Hotel confortável e bem localizado.',
-    type: 'hotel',
-  },
-];
+// const points: Point[] = [
+//   {
+//     id: 1,
+//     name: 'Restaurante Saboroso',
+//     position: [-3.024930, -39.649836],
+//     image: 'https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+//     description: 'Um restaurante incrível no centro.',
+//     type: 'restaurant',
+//   },
+//   {
+//     id: 2,
+//     name: 'Bar do Zé',
+//     position: [-3.024860, -39.643643],
+//     image: 'https://plus.unsplash.com/premium_photo-1661730134261-4381db9b5de3?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+//     description: 'O melhor happy hour da cidade.',
+//     type: 'bar',
+//   },
+//   {
+//     id: 3,
+//     name: 'Hotel Confort',
+//     position: [-3.034040, -39.654925],
+//     image: 'https://images.unsplash.com/photo-1551882547-ff40c63fe5fa?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+//     description: 'Hotel confortável e bem localizado.',
+//     type: 'hotel',
+//   },
+// ];
 
 function ShowLatLongOnClick() {
   const map = useMap()
@@ -104,6 +106,23 @@ export default function Map() {
     null
   )
 
+  const [places, setPlaces] = useState<Place[]>([])
+
+  useEffect(() => {
+    const fetchPlaces = async () => {
+
+      try{
+        //npm i axios
+        const response = await axios.get<Place[]>("http://localhost:3001/places")
+        setPlaces(response.data)
+      }catch(err){
+        console.log("Erro ao carregar os locais: ", err)
+      }
+
+    }
+    fetchPlaces();
+  })
+
   return (
     <MapContainer
       center={[-3.029350, -39.653422]}
@@ -115,28 +134,6 @@ export default function Map() {
         attribution="&copy; OpenStreetMap"
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-
-      {points.map((point) => (
-        <Marker
-          key={point.id}
-          position={point.position}
-          icon={iconMap[point.type]}
-        >
-          <Popup>
-            <div className="text-center">
-              <img
-                src={point.image}
-                alt={point.name}
-                className="w-full h-24 object-cover rounded mb-2"
-              />
-              <h2 className="font-semibold">{point.name}</h2>
-              <p className="text-sm">{point.description}</p>
-            </div>
-          </Popup>
-        </Marker>
-      ))}
-
-      {/* <ShowLatLongOnClick/> */}
 
       <ShowPlaceFormOnClick setFormPosition={setFormPosition}/>
 
@@ -153,12 +150,36 @@ export default function Map() {
         >
           <Popup>
             <PlaceForm 
-            lat={parseFloat(formPosition[0].toFixed(2))} 
-            lng={parseFloat(formPosition[1].toFixed(2))}/>
+            lat={formPosition[0]} 
+            lng={formPosition[1]}/>
           </Popup>
         </Marker>
       )}
 
+
+      {places.map((place)=>(
+        <Marker
+        key={place.id}
+        position={[place.latitude, place.longitude]}
+        icon={iconMap[place.type]}
+        >
+          <Popup>
+            <div className='text-center'>
+            {place.images?.[0] && (
+              <img 
+                src={place.images[0].url} 
+                alt={place.name} 
+                className='w-full h-24 object-cover rounded mb-2'
+                />
+              )}
+              <h2 className='font-semibold mx-10'>{place.name}</h2>
+              <p className='text-sm'>{place.phone}</p>
+            </div>
+          </Popup>
+
+
+        </Marker>
+      ))}
     </MapContainer>
   );
 }
--- src/components/PlaceForm.tsx
+++ src/components/PlaceForm.tsx
@@ -3,10 +3,13 @@
 import { useForm } from "react-hook-form"
 import { z } from "zod"
 import { zodResolver } from "@hookform/resolvers/zod"
+import { useEffect, useState } from "react";
+
+import axios from "axios";
 
 const placeValidationSchema = z.object({
     name: z.string().min(3, "Nome muito curto!"),
-    type: z.enum(["restaurante", "bar", "hotel"]),
+    type: z.enum(["RESTAURANTE", "BAR", "HOTEL"]),
     phone: z.string().min(8, "Telefone inválido"),
     lat: z.number(),
     lng: z.number()
@@ -24,7 +27,8 @@ export default function PlaceForm({lat, lng}: Props) {
     const {
         register,
         handleSubmit,
-        formState: { errors, isSubmitSuccessful }
+        formState: { errors, isSubmitSuccessful },
+        reset,
     } = useForm<PlaceFormData>({
         resolver: zodResolver(placeValidationSchema),
         defaultValues: {
@@ -33,9 +37,61 @@ export default function PlaceForm({lat, lng}: Props) {
         }
     })
 
-    const onSubmit = (data: PlaceFormData) => {
-        alert("Dados do local enviados!")
+    const onSubmit = async (data: PlaceFormData) => {
+        
+        if(!images || images.length===0) {
+            setSubmitError("Pelo uma imagem deve ser enviada !")
+            return
+        }
+
+        if(images.length > 3) {
+            setSubmitError("Você pode enviar no máximo 3 imagens!")
+            return
+        }
+
+        const formData = new FormData();
+        formData.append("name", data.name)
+        formData.append("type", data.type)
+        formData.append("phone", data.phone)
+        formData.append("latitude", String(data.lat))
+        formData.append("longitude", String(data.lng))
+
+        Array.from(images).forEach((file) => {
+            formData.append("images", file)
+        })
+
+        try {
+            // Faço a requisição
+            setLoading(true)
+            const response = await axios.post(
+                "http://localhost:3001/places",
+                formData, 
+                {
+                    headers: {"Content-Type": "multipart/form-data"}
+                })
+            reset();
+            setImages(null);
+        } catch(error) {
+            setSubmitError("Erro ao enviar o formulário!")
+        } finally {
+            setLoading(false)
+        }
+
+
+
     }
+
+    useEffect(() => {
+        reset((prev) => ({
+            ...prev,
+            lat,
+            lng
+        }))
+    }, [lat, lng, reset])
+
+    const [images, setImages ] = useState<FileList | null>(null)
+    const [submitError, setSubmitError] = useState<String | null>(null)
+    const [loading, setLoading] = useState(false)
 
     return (
         <form className="space-y-4" onSubmit={handleSubmit(onSubmit)}>
@@ -56,9 +112,9 @@ export default function PlaceForm({lat, lng}: Props) {
                 {...register("type")}
             >
                 <option>Selecione</option>
-                <option value="restaurante">Restaurante</option>
-                <option value="bar">Bar</option>
-                <option value="hotel">Hotel</option>
+                <option value="RESTAURANTE">Restaurante</option>
+                <option value="BAR">Bar</option>
+                <option value="HOTEL">Hotel</option>
             </select>
             {errors.type && <p className="text-red-500">
                 {errors.type.message}
@@ -81,6 +137,7 @@ export default function PlaceForm({lat, lng}: Props) {
             <div>
                 <label>Latitude: </label>
                 <input
+                    {...register("lat")}
                     value={lat}
                     readOnly
                     className="border rounded px-2 py-1 bg-gray-100"
@@ -91,9 +148,22 @@ export default function PlaceForm({lat, lng}: Props) {
             <div>
                 <label>Longitude: </label>
                 <input
+                    {...register("lng")}
                     value={lng}
                     readOnly
                     className="border rounded px-2 py-1 bg-gray-100"
+                />
+            </div>
+
+            {/* Imagens */}
+            <div>
+                <label>Imagens</label>
+                <input
+                 type="file"
+                 accept="image/*"
+                 multiple
+                 onChange={(e)=>setImages(e.target.files)}
+                 className="w-full border rounded px-2 py-1"
                 />
             </div>
 
@@ -103,8 +173,18 @@ export default function PlaceForm({lat, lng}: Props) {
                 className="bg-blue-600 text-white 
                 px-4 py-1 rounded hover:bg-blue-700
                 cursor-pointer">
-                Registrar Local
+                {loading ? "Enviando..." : "Registrar Local" }
             </button>
+
+            {isSubmitSuccessful && !submitError && (
+                <p className="text-green-400 mt-2">
+                    Formulário enviado com sucesso
+                </p>
+            ) }
+
+            {submitError && (
+                <p className="text-red-400 mt-2" >{submitError}</p>
+            )}
 
         </form>
     )
