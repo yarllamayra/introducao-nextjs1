49d53195b3f2f368d98676320c0b6975e2db19d7
Yarlla <oliveirayarlla22@gmail.com>
2025-08-14 21:48:47 -0300

    implementando operações mistas

--- src/components/ui/miniDisplay.tsx
+++ src/components/ui/miniDisplay.tsx
@@ -1,95 +1,172 @@
-"use client";
+"use client"
+import { useState } from "react"
 
-import { useState } from "react";
 
 export default function MiniDisplay() {
-  const [display, setDisplay] = useState("");
 
-  const calculate = () => {
-    let result = 0;
+    const [display, setDisplay] = useState("")
 
-    if (display.includes("+")) {
-      const values = display.split("+").map(Number);
-      result = values.reduce((a, b) => a + b, 0);
-    } else if (display.includes("-")) {
-      const values = display.split("-").map(Number);
-      result = values.slice(1).reduce((a, b) => a - b, values[0]);
-    } else if (display.includes("*")) {
-      const values = display.split("*").map(Number);
-      result = values.reduce((a, b) => a * b, 1);
-    } else if (display.includes("/")) {
-      const values = display.split("/").map(Number);
-      result = values.slice(1).reduce((a, b) => a / b, values[0]);
+    const backspace = () => {
+        setDisplay(display.slice(0,-1))
     }
 
-    setDisplay(String(result));
-  };
+    const calculate = () => {
+    try {
+        const expression = display.replace(/\s+/g, "");
+        const result = new Function("return " + expression)();
+        setDisplay(String(result));
+    } catch (error) {
+        setDisplay("Erro");
+    }
+}
 
-  return (
-    <div>
-      <div className="flex flex-col items-center mt-10">
-        <div className="flex justify-center items-center border border-gray-400 rounded px w-40 h-12 text-center text-xl overflow-x-auto">
-          {display}
+    const sun = () => {
+        const values = display.split("+")
+        const result = values.reduce(
+            (acumuladora, valorAtual) => acumuladora + Number(valorAtual),
+            0
+        )
+        setDisplay(String(result))
+    }
+
+    const subtraction = () => {
+        const values = display.split("-")
+        const numberValues = values.map((value)=>Number(value))
+        const result = numberValues.reduce(
+            (acumuladora, valorAtual) => acumuladora - valorAtual
+        )
+        setDisplay(String(result))
+    }
+
+    const product = () => {
+        const values = display.split("*")
+        const numberValues = values.map((value)=>Number(value))
+        const result = numberValues.reduce(
+            (acumuladora, valorAtual) => acumuladora * valorAtual
+        )
+        setDisplay(String(result))
+    }
+
+    const division = () => {
+        const values = display.split("/")
+        const numberValues = values.map((value)=>Number(value))
+        const result = numberValues.reduce(
+            (acumuladora, valorAtual) => acumuladora / valorAtual
+        )
+        setDisplay(String(result))
+    }
+
+    return (
+        <div className="flex flex-col items-center my-10">
+            
+            {/* display */}
+            <div className="flex justify-center overflow-x-auto
+                items-center border border-gray-400 rounded
+                p-4 w-40 h-14 text-center text-xl bg-gray-50 shadow-md">
+                {display}
+            </div>
+
+            {/* teclado numérico */}
+            <div className="grid grid-cols-3 gap-2 w-40 h-40 mt-5">
+                <button 
+                className="bg-blue-300 hover:bg-blue-400 text-white font-semibold p-2 rounded shadow"
+                onClick={() =>{ setDisplay(display + "0") }}
+                >
+                    0
+                </button>
+                <button 
+                onClick={() =>{ setDisplay(display + "1") }}
+                className="bg-blue-300 hover:bg-blue-400 text-white font-semibold p-2 rounded shadow">
+                    1
+                </button>
+                <button 
+                onClick={() =>{ setDisplay(display + "2") }}
+                className="bg-blue-300 hover:bg-blue-400 text-white font-semibold p-2 rounded shadow">
+                    2
+                </button>
+                <button 
+                onClick={() =>{ setDisplay(display + "3") }}
+                className="bg-blue-300 hover:bg-blue-400 text-white font-semibold p-2 rounded shadow">
+                    3
+                </button>
+                <button 
+                onClick={() =>{ setDisplay(display + "4") }}
+                className="bg-blue-300 hover:bg-blue-400 text-white font-semibold p-2 rounded shadow">
+                    4
+                </button>
+                <button 
+                onClick={() =>{ setDisplay(display + "5") }}
+                className="bg-blue-300 hover:bg-blue-400 text-white font-semibold p-2 rounded shadow">
+                    5
+                </button>
+                <button 
+                onClick={() =>{ setDisplay(display + "6") }}
+                className="bg-blue-300 hover:bg-blue-400 text-white font-semibold p-2 rounded shadow">
+                    6
+                </button>
+                <button 
+                onClick={() =>{ setDisplay(display + "7") }}
+                className="bg-blue-300 hover:bg-blue-400 text-white font-semibold p-2 rounded shadow">
+                    7
+                </button>
+                <button 
+                onClick={() =>{ setDisplay(display + "8") }}
+                className="bg-blue-300 hover:bg-blue-400 text-white font-semibold p-2 rounded shadow">
+                    8
+                </button>
+                <button 
+                onClick={() =>{ setDisplay(display + "9") }}
+                className="bg-blue-300 hover:bg-blue-400 text-white font-semibold p-2 rounded shadow">
+                    9
+                </button>
+
+                <button 
+                onClick={() =>{ setDisplay("")}}
+                className="bg-violet-300 hover:bg-violet-400 text-white font-semibold p-2 rounded shadow">
+                 C
+                </button>
+
+                {/* Botão Soma */}
+                <button 
+                onClick={() =>{setDisplay(`${display}+`)}}
+                className="bg-green-500 hover:bg-green-600 text-white font-semibold p-2 rounded shadow">
+                 +
+                </button>
+
+                {/* Botão Multiplicação */}
+                <button 
+                onClick={() =>{setDisplay(`${display}*`)}}
+                className="bg-green-500 hover:bg-green-600 text-white font-semibold p-2 rounded shadow">
+                 *
+                </button>
+                
+                {/* Botão Divisão */}
+                <button
+                onClick={() =>{setDisplay(`${display}/`)}}
+                className="bg-green-500 hover:bg-green-600 text-white font-semibold p-2 rounded shadow">
+                /
+                </button>
+                
+
+                <button 
+                onClick={() =>{calculate()}}
+                className="bg-amber-400 hover:bg-amber-500 text-white font-semibold p-2 rounded shadow">
+                 =
+                </button>
+
+                <button 
+                onClick={() =>{setDisplay(`${display}-`)}}
+                className="bg-green-400 hover:bg-green-500 text-white font-semibold p-2 rounded shadow">
+                 -
+                </button>
+
+                <button 
+                onClick={() =>{backspace()}}
+                className="bg-violet-300 hover:bg-violet-400 text-white font-semibold p-2 rounded shadow col-span-2">
+                 backspace
+                </button>
+            </div>
+
         </div>
-
-        <div className="grid grid-cols-3 gap-2 w-40 h-40 mt-5">
-          {["0","1","2","3","4","5","6","7","8","9"].map((num) => (
-            <button
-              key={num}
-              className="bg-blue-300 p-2 rounded"
-              onClick={() => setDisplay(display + num)}
-            >
-              {num}
-            </button>
-          ))}
-
-          <button
-            onClick={() => setDisplay(display + "+")}
-            className="bg-green-500 p-2 rounded"
-          >
-            +
-          </button>
-
-          <button
-            onClick={() => setDisplay(display + "-")}
-            className="bg-gray-400 p-2 rounded"
-          >
-            -
-          </button>
-
-          <button
-            onClick={() => setDisplay(display + "*")}
-            className="bg-yellow-500 p-2 rounded"
-          >
-            *
-          </button>
-
-          <button
-            onClick={() => setDisplay(display + "/")}
-            className="bg-red-500 p-2 rounded"
-          >
-            /
-          </button>
-
-          <button onClick={calculate} className="bg-amber-600 p-2 rounded">
-            =
-          </button>
-
-          <button
-            onClick={() => setDisplay("")}
-            className="bg-purple-500 p-2 rounded"
-          >
-            C
-          </button>
-
-          <button
-            onClick={() => setDisplay(display.slice(0, -1))}
-            className="bg-violet-300 p-2 rounded col-span-2"
-          >
-            Backspace
-          </button>
-        </div>
-      </div>
-    </div>
-  );
+    )
 }
